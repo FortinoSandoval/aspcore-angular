c0f9e561e10fa3d18dcbd5c005916bdc4f5b0b9d
Fortino Sandoval <fortino.sandoval@maritz.com>
2018-11-20 12:11:19 -0800

    swagger added

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using AutoMapper;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace aspcore_angular
 {
@@ -33,6 +34,12 @@ namespace aspcore_angular
       {
         configuration.RootPath = "ClientApp/dist";
       });
+
+      // Register the Swagger generator, defining 1 or more Swagger documents
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new Info { Title = "Vehicles API", Version = "v1" });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +58,16 @@ namespace aspcore_angular
       app.UseHttpsRedirection();
       app.UseStaticFiles();
       app.UseSpaStaticFiles();
+
+      // Enable middleware to serve generated Swagger as a JSON endpoint.
+      app.UseSwagger();
+
+      // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+      // specifying the Swagger JSON endpoint.
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+      });
 
       app.UseMvc(routes =>
       {
